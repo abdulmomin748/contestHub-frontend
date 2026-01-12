d0730a03a32961dab2dbeebc3156c94382895df6
Ab Momin <mominurrahman10748@gmail.com>
2026-01-12 19:44:49 +0600

    more updated

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 import useAuth from "./useAuth";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://contest-hub-backend-taupe.vercel.app",
   withCredentials: true,
 });
 
